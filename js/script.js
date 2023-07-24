5c39fbc466086ea3c590f7ee380a4dd0547b92b0
pyrz1k <139967486+pyrz1k@users.noreply.github.com>
2023-07-24 16:35:34 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let changeTheme = document.getElementById("changeTheme");
-
-changeTheme.onclick = function(){
-    let theme = document.getElementById("theme");
-
-    if (theme.getAttribute("href") == "light.css") {
-        theme.href = "dark.css"
-    }
-}
